211fcf5f2275d3d398038b12dd41a1e96514763f
Edgecase Pairing <pairing@edgecase.com>
2014-04-08 22:54:28 -0400

    layout version 2

--- js/app.js
+++ js/app.js
@@ -1,12 +1,27 @@
 Patterns = Ember.Application.create();
 
 Patterns.Router.map(function() {
-  this.resource('week_one');
+  this.resource('index', { path: '/' });
   this.resource('week_two');
   this.resource('week_three');
   this.resource('week_four');
   this.resource('week_five');
+  this.resource('week_one');
+  this.resource('growth_mindset');
+  this.resource('emptying_the_cup');
+  this.resource('construct_your_own_curriculm');
+  this.resource('exposing_your_ignorance');
+  this.resource('sweep_the_floor');
+  this.resource('walking_the_long_road');
+  this.resource('accurate_self_assessment');
+  this.resource('perpetual_learning');
+  this.resource('a_different_road');
+  this.resource('draw_your_own_map');
+  this.resource('all_posts');
 });
+
 
 Patterns.IndexRoute = Ember.Route.extend({
 });
+
+
