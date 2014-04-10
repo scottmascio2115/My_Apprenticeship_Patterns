87dd1c3a9b5ae0c81ee14d1a6765f6a20115631c
Scott Mascio <scottmascio@gmail.com>
2014-04-10 09:42:04 -0400

    week six inclusion

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ Patterns.Router.map(function() {
   this.resource('week_four');
   this.resource('week_five');
   this.resource('week_one');
+  this.resource('week_six');
   this.resource('growth_mindset');
   this.resource('emptying_the_cup');
   this.resource('construct_your_own_curriculm');
@@ -17,6 +18,7 @@ Patterns.Router.map(function() {
   this.resource('perpetual_learning');
   this.resource('a_different_road');
   this.resource('draw_your_own_map');
+  this.resource('confronting_your_ignorance');
   this.resource('all_posts');
 });
 
